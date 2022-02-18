fc96e142ddf7550b3afcddb79c2f3f297cd11101
soymi1k <soymilkneigh@gmail.com>
2022-02-18 17:56:30 +0900

    refactoring structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let d = new Date();
+var thisDay = new Date();
 var weekDay = new Array();
 
 weekDay[0] = "(일) 10:00 ~ 14:30";
@@ -9,8 +9,7 @@ weekDay[4] = "(목) 10:00 ~ 21:00 야간진료";
 weekDay[5] = "(금) 10:00 ~ 19:00";
 weekDay[6] = "(토) 10:00 ~ 14:30";
 
-document.getElementById("date").innerHTML =
-  "오늘의 진료시간 " + weekDay[d.getDay()];
-
-document.getElementById("date02").innerHTML =
-  "오늘의 진료시간 " + weekDay[d.getDay()];
+var elems = document.getElementsByClassName("date");
+for (var i = 0; i < elems.length; i++) {
+  elems[i].innerHTML = "오늘의 진료시간 " + weekDay[thisDay.getDay()];
+}
